8b116094b689e68548d3c2fc3f0aa05dbecb2365
Shashwat <shashwat@Shashwats-MacBook-Air.local>
2025-02-17 09:48:59 +0530

    using parse and stringify

--- abes/http/http_json.js
+++ abes/http/http_json.js
@@ -3,7 +3,8 @@ const fs = require ('fs/promises');
 
 const server = http.createServer(async(req,res)=>{
     res.writeHead(200,{'Content-Type': 'application/json'});
-    const filedata = await fs.readFile("./new.json", "utf-8")
+    const filedata = await fs.readFile("./new.json", "utf-8");
+    const parsedata = JSON.parse(filedata);
     // const newData = users.map((user)=>{
     //     return user.name
     // });//will take data of name of each user in the users array and create a new array with just the names
@@ -11,7 +12,7 @@ const server = http.createServer(async(req,res)=>{
     // users.forEach((user)=>{
     //     newData+=`${user.name} `;
     // })
-    res.end(filedata) // return the JSON data in form a string 
+    res.end(JSON.stringify(filedata)) // return the JSON data in form a string 
 });
 PORT = 3000;
 server.listen(PORT, ()=>{
