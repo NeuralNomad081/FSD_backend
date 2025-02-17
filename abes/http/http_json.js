8ef40091aa90a876ff1c7e0a69ba80913b2e50ee
Shashwat <shashwat@Shashwats-MacBook-Air.local>
2025-02-17 09:46:08 +0530

    import and print json data from another file

--- abes/http/http_json.js
+++ abes/http/http_json.js
@@ -1,20 +1,17 @@
 const http = require ('http');
-const users = [
-    
-    {id: 1, name: "shashwat", email:"shashwatp011@gmail.com"},
-    {id:2, name: "sarthak", email:'sarthak@gmail.com'},
-    {id:3, name:'khushi', email:'khushi@gmail.com'}
-]
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type': 'text/html'});
+const fs = require ('fs/promises');
+
+const server = http.createServer(async(req,res)=>{
+    res.writeHead(200,{'Content-Type': 'application/json'});
+    const filedata = await fs.readFile("./new.json", "utf-8")
     // const newData = users.map((user)=>{
     //     return user.name
     // });//will take data of name of each user in the users array and create a new array with just the names
-    let newData = ' ';
-    users.forEach((user)=>{
-        newData+=`${user.name} `;
-    })
-    res.end(newData) // return the JSON data in form a string 
+    // let newData = ' ';
+    // users.forEach((user)=>{
+    //     newData+=`${user.name} `;
+    // })
+    res.end(filedata) // return the JSON data in form a string 
 });
 PORT = 3000;
 server.listen(PORT, ()=>{
