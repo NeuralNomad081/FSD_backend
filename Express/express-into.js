0e19d9d0bb15a8972ffc4919cab1ad56d478c918
Code081 <shashwatp011@gmail.com>
2025-02-24 09:58:42 +0530

    learned about try catch in js and more

--- Express/express-into.js
+++ Express/express-into.js
@@ -4,7 +4,17 @@ server.get('/api', (req,res)=>{
     try {
     //using object destructuing
     const {name , rollNo } = req.query; 
-    res.send(`Hello Mr. ${name} and your roll number is ${rollNo}`);
+    if (!name){
+        // res.send({
+        //     status:404, message : "Name is required"
+        // });
+        //or
+        res.status(404).send("Please give a name")
+    }
+    else{
+        res.send(`Hello Mr. ${name} your roll number is ${rollNo}`);
+        // correct url : http://localhost:3000/api?name=shashwat&rollNo=57
+    }
     } catch (error) {
         console.log(error.message);    
     }
