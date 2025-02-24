1fefe5905a92577969d433b6885e8ac55b2adc3c
Code081 <shashwatp011@gmail.com>
2025-02-24 09:52:54 +0530

    some changes

--- Express/express-into.js
+++ Express/express-into.js
@@ -1,9 +1,13 @@
 import express from 'express';
 const server = express();
 server.get('/api', (req,res)=>{
+    try {
     //using object destructuing
-    const {name = "Jhon Doe", rollNo = 100} = req.query; //we can also use by default values
+    const {name , rollNo } = req.query; 
     res.send(`Hello Mr. ${name} and your roll number is ${rollNo}`);
+    } catch (error) {
+        console.log(error.message);    
+    }
 });
 
 const PORT = 3000;
